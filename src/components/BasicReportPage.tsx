44d41d87955c8d66b9f78a18f66e4e3883a158b3
Jessica-C2004 <jacunnin@udel.edu>
2024-04-22 11:13:14 -0400

    added temp picture to BasicReportPage

--- src/components/BasicReportPage.tsx
+++ src/components/BasicReportPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import "./pages.css";
 import { AIKey } from '../interfaces/AIKeyInterface';
 import { Col, Container, Row, Image} from 'react-bootstrap';
-import logo from "../logoandimages/thecareerhelpilogo.png";
+import logo from "../logoandimages/cowboy.jpg";
 
 export function BasicReport(key: AIKey): JSX.Element {
     return <div className="Pages" id="Basic Questions Report Page">
@@ -11,11 +11,13 @@ export function BasicReport(key: AIKey): JSX.Element {
             <Row>
                 <Col>
                 <div>
-                    <Image src={logo} thumbnail></Image>
+                    <Image src={logo} alt="career-picture" thumbnail></Image>
                 </div>
                 </Col>
                 <Col>
-                <div> Report text here</div>
+                <div> Report text here: <br></br>
+                    This will include a simple description of the career and some info on why that was the chosen one. Will be updated when integrating openAI
+                </div>
                 </Col>
             </Row>
         </Container>
