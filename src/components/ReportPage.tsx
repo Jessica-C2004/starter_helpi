795cf48bc10cd991aef1f1f729b3f7ff56b778ff
Jessica-C2004 <jacunnin@udel.edu>
2024-05-05 18:35:17 -0400

    moved function up and modified basicpage nav (prob will go back)

--- src/components/ReportPage.tsx
+++ src/components/ReportPage.tsx
@@ -4,11 +4,39 @@ import logo from "../logoandimages/cowboy.jpg";
 import { OpenAI } from 'openai';
 // import { useState } from "react";
 
+async function generateCareer() {
+    const key = localStorage.getItem("MYKEY")?.replace(/['"]+/g, '');
+    if (key === null) {
+        console.log("No key found");
+        return;
+    }
+
+    const questions = localStorage.getItem("questions");
+    const answers = localStorage.getItem("answers");
+
+    console.log("Questions: ", questions);
+    console.log("Answers: ", answers);
+
+    const openai = new OpenAI({ apiKey: key , dangerouslyAllowBrowser: true});
+    const completion = await openai.chat.completions.create({
+         model: 'gpt-3.5-turbo', // gpt-3.5-turbo, gpt-4
+         max_tokens: 128,
+         messages: [
+             { role: 'system', content: "" },
+             { role: 'user', content: "Please respond with all the letters in the alphabet" }
+         ]
+     });
+     let content = completion.choices[0]?.message?.content?.trim() ?? '';
+     console.log('OpenAI Output: \n', content);
+}
+
+
 export function Report(): JSX.Element {
 
+    console.log(localStorage.getItem("MYKEY"))
     // const [reportVisible, setReportVisible] = useState<boolean>(false);
 
-    return <div className="Pages" id="Basic Questions Report Page">
+    return <div className="Pages">
         <h1>Your Suggested Career is...</h1>
         <Button onClick={() => generateCareer()}>Generate Report</Button>
         <Container>
@@ -26,30 +54,4 @@ export function Report(): JSX.Element {
             </Row>
         </Container>
     </div>;    
-}
-
-async function generateCareer() {
-    const key = localStorage.getItem("MYKEY")?.replace(/['"]+/g, '');
-    if (key === null) {
-        console.log("No key found");
-        return;
-    }
-
-    const questions = localStorage.getItem("questions");
-    const answers = localStorage.getItem("answers");
-
-    console.log("Questions: ", questions);
-    console.log("Answers: ", answers);
-
-    // const openai = new OpenAI({ apiKey: key , dangerouslyAllowBrowser: true});
-    // const completion = await openai.chat.completions.create({
-    //     model: 'gpt-3.5-turbo', // gpt-3.5-turbo, gpt-4
-    //     max_tokens: 128,
-    //     messages: [
-    //         { role: 'system', content: "" },
-    //         { role: 'user', content: "Please respond with all the letters in the alphabet" }
-    //     ]
-    // });
-    // let content = completion.choices[0]?.message?.content?.trim() ?? '';
-    // console.log('OpenAI Output: \n', content);
 }
