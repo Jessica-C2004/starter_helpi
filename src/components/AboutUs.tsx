090f3dba6df6e518e239ae116af960d60be48244
Sydney Holland <sydneymh@udel.edu>
2024-05-19 16:20:52 -0400

    Inclusion of info and resizing elements

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -28,13 +28,43 @@ export function AboutUs(): JSX.Element {
         <div className="Pages">
                 <div className="About-us">
                     <Row>
-                    <h1 className="About-title">Meet the Team!</h1>
+                    <h1 className="Page-title">Meet the Team!</h1>
                     </Row>
                 </div>
                     <Row>
                         <Col> 
-                        <div className="About-person">
+                        <div className="person-name">
                             Jessica Cunningham
+                            </div>
+                            <div className="person-email">
+                                Email: jacunnin@udel.edu
+                                </div>
+                                <div className="person-github">
+                                Github: Jessica-C2004
+                            </div>
+                            <div className="person-name">
+                            Patrick Sweet
+                            </div>
+                            <div className="person-email">
+                            </div>
+                        </Col>
+                        <Col>
+                        <div className="person-name">
+                            Melanie Heider
+                            </div>
+                            <div className="person-email">
+                                Email: msheider@udel.edu
+                                </div>
+                                <div className="person-github">
+                                Github: msheider
+                            </div>
+                            <div className="person-name">
+                            Sydney Holland</div>
+                            <div className="person-email">
+                                Email: sydneymh@udel.edu
+                                </div>
+                                <div className="person-github">
+                                Github: sydneyh9
                             </div>
                         </Col>
                     </Row>
